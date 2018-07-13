615b17d6a32871168f999fe53ae203750a5ee50d
David Athobe <dathobek@gmail.com>
2018-07-13 20:22:37 +0100

    prod-ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig : true
+  firebaseConfig : {
+    apiKey: "AIzaSyBOhwX0Jt9JaGGS5Eqlz4zolZo4RWfy9SI",
+    authDomain: "vbc-reg.firebaseapp.com",
+    databaseURL: "https://vbc-reg.firebaseio.com",
+    projectId: "vbc-reg",
+    storageBucket: "vbc-reg.appspot.com",
+    messagingSenderId: "665095492386"
+  }
 };
